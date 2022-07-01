8ec9070b64cfa70bde339cf5a0e181b85c2c830a
Liam Hopkins <liamh@google.com>
2022-07-01 12:38:17 -0700

    add golang

--- base/build.sh
+++ base/build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -e
+
 useradd -u 1001 -m developer
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y git vim
+apt-get install -y git vim curl
 apt-get clean
 
 su - developer /user.sh
--- base/user.sh
+++ base/user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -x
+set -ex
 
 git clone --depth 1 https://github.com/hopkiw/dotfiles
 cd dotfiles
@@ -11,5 +11,10 @@ git clone https://github.com/tomasr/molokai
 mkdir .vim
 mv molokai/colors .vim/
 rm -rf molokai
+mkdir goroot
+curl -s "https://dl.google.com/go/go1.18.3.linux-amd64.tar.gz" -o go.tar.gz
+tar -C ~/goroot --strip-components=1 -xf go.tar.gz
+rm go.tar.gz
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vi +PluginInstall +qall  >/dev/null </dev/null
+vi +GoInstallBinaries +qall  >/dev/null </dev/null
